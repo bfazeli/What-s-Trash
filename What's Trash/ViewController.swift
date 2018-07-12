6f0e72d5fda3f1b6b3947ca812368b486edd371f
bijan102 <bfazeli1@gmail.com>
2018-07-12 00:29:49 -0700

    Update: Hooked up necessary comp and set up camera permission

--- What's Trash/ViewController.swift
+++ What's Trash/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let usrPickedImg = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = usrPickedImg
+        } else {
+            fatalError()
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
-
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+       present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
